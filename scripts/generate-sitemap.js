12e78b53cadf70126da636e3173a071d256fa5b4
Praveen Yadav <pyadav9678@gmail.com>
2021-07-31 22:00:42 +0530

    refactor: sitemap generator script

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -5,8 +5,12 @@ const prettier = require("prettier");
 (async () => {
   const prettierConfig = await prettier.resolveConfig("./.prettierrc");
   const pages = await globby([
-    "pages/**/*{.tsx,.mdx}",
+    "pages/**/*.tsx",
+    "content/**/*{.md,.mdx}",
     "!pages/_*.tsx",
+    "!pages/404.tsx",
+    "!pages/500.tsx",
+    "!pages/post/[...slug].tsx",
     "!pages/api",
   ]);
   const sitemap = `
@@ -16,13 +20,20 @@ const prettier = require("prettier");
               .map((page) => {
                 const path = page
                   .replace("pages", "")
-                  .replace(".js", "")
+                  .replace("content", "")
+                  .replace(".tsx", "")
                   .replace(".mdx", "");
                 const route = path === "/index" ? "" : path;
                 return `
-                        <url>
-                            <loc>${`http://iamyadav.com${route}`}</loc>
-                        </url>
+                      <url>
+                          <loc>${`https://iamyadav.com${route}`}</loc>
+                          <changefreq>${
+                            route.includes("/post") ? "monthly" : "daily"
+                          }</changefreq>
+                          <priority>${
+                            route.includes("/post") ? 0.7 : 0.2
+                          }</priority>
+                      </url>   
                     `;
               })
               .join("")}
